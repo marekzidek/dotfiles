7e11dd63a368ce30dbd5cfb7e20260680676d598
Marek Židek <marekzidek@centrum.cz>
2020-07-22 19:13:48 +0200

    Add very WIP install/bootstrap script

---
+++ install_script.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+# Manual settings
+Use iterm2 and set colorscheme as Elemental, and set
+the foreground color to normal white instead of grey
+
+# From system repo
+fzf
+tldr
+zsh
+tmux
+ranger
+bat
+rg
+
+# Configs
+chsh -s $(which zsh)
+
+# Pypi
+virtualenv
+scikitlearn
+numpy
+pandas
+
+
+###### MacOS Section ########
+hammerspoon
+homebrew
+karabiner
+
