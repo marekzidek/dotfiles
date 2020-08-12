054077bb142f2786c16fe0354b6fa9ed6e8ca8b9
Marek Zidek <marekzidek@centrum.cz>
2020-08-12 12:45:18 +0200

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -1,27 +1,52 @@
 #!/bin/bash
 
+# Create custom user with sudo priviledges
+	#useradd marekzide
+	#usermod -aG sudo marekzidek
+
+# For retarded images with no manual pages
+	# vim /etc/dpkg/dpkg.cfg.d/excludes
+	# comment the line with: path-exclude=/usr/share/man/*
+	# sudo apt-get --reinstall install man-db coreutils
+	# continue with your life
+
+# Manual installs
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+#iterm
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 
 # Manual settings
 Use iterm2 and set colorscheme as Elemental, and set
 the foreground color to normal white instead of grey
 
 # From system repo
+man
+neovim
 fzf
+python3-pip
 tldr
 zsh
 tmux
 ranger
 bat
-rg
+ripgrep
+autojump
+libtool
+autoconf
+automake
+
 
 # Configs
 chsh -s $(which zsh)
 
+
 # Pypi
 virtualenv
+matplotlib
 scikitlearn
 numpy
 pandas
+torch
 
 
 ###### MacOS Section ########
