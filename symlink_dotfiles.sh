548f47359f17a5bb40f8b392f8e0f317ca5f0b1f
Marek Zidek <marekzidek@centrum.cz>
2020-08-18 10:35:33 +0200

    Update symlink_dotfiles to symling ipython config, remove nvim support

--- symlink_dotfiles.sh
+++ symlink_dotfiles.sh
@@ -1,3 +1,8 @@
 ln -s $(pwd)/.zshrc ~/.zshrc
 ln -s $(pwd)/.vimrc ~/.vimrc
 ln -s $(pwd)/.tmux.conf ~/.tmux.conf
+
+# Just don't use neovim... it's shit, remember that forever
+#mkdir -p ~/.config/nvim
+#ln -s $(pwd)/nvim/init.vim ~/.config/nvim/init.vim
+ln -s $(pwd)/ipython/profile_default/startup/keybindings.py ~/.ipython/profile_default/startup/keybindings.py
